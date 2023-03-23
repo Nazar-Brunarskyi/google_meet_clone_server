8c32191e0eab7009f87d03fa58968351ced0b11f
Nazar Brunarskyi <3nnazar836@gmail.com>
2023-03-23 13:45:45 +0200

    cors is added

--- src/server.ts
+++ src/server.ts
@@ -1,10 +1,11 @@
 import express from 'express';
 import { usersRouter } from './routes/users';
+import cors from 'cors';
 
 const PORT = 5000;
-
 const app = express();
 
+app.use(cors());
 app.use(usersRouter);
 
 app.listen(PORT, () => {
