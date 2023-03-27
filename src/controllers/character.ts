6892ab71e3780ffeed69e2cd6d3a4058a23a5c91
Nazar Brunarskyi <3nnazar836@gmail.com>
2023-03-27 09:29:13 +0300

    removed spaces

--- src/controllers/character.ts
+++ src/controllers/character.ts
@@ -5,7 +5,6 @@ export const getCharacters = async (req: Request, res: Response) => {
   try {
     const data = await charactersServices.getCharacters(12);
 
-    
     res.send(data)
   } catch (err) {
     res.sendStatus(500);
@@ -16,7 +15,6 @@ export const getRandomCharacter = async (req: Request, res: Response) => {
   try {
     const data = await charactersServices.getCharacter();
 
-    
     res.send(data)
   } catch (err) {
     res.sendStatus(500);
